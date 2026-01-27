dc3efe10856b417edd09d13d75a20ff660ee10b0
Anyars Yussif <anyarsencarta@gmail.com>
2026-01-27 08:14:33 +0000

    Add data table to subjects list

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const DEPARTMENTS = [
+  "Mathematics",
+  "Science",
+  "History",
+  "Language Arts",
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept,
+    label: dept,
+}));
+
+export const subjects = [
+  "Mathematics",
+  "Science",
+  "History",
+  "Language Arts",
+];

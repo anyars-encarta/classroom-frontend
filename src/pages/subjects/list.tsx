b116b2a2c84d9e83eff66fe6750bb9bf49f8f69d
Anyars Yussif <anyarsencarta@gmail.com>
2026-02-03 07:23:15 +0000

    Correct string type in department header field

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -75,7 +75,7 @@ const SubjectsList = () => {
                 column={column}
                 defaultOperator="eq"
                 options={
-                  departments?.map(({ name }: { name: string }) => ({
+                  departments?.map(( name: string ) => ({
                     label: name,
                     value: String(name),
                   })) || []
